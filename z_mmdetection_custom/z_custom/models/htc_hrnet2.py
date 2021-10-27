9b15ad357547ef64a002db7de4e63608ca1502f5
simbean <sb9338@naver.com>
2021-10-27 15:56:45 +0900

    20211027, 0.712LB 12epoch

---
+++ z_mmdetection_custom/z_custom/models/htc_hrnet2.py
@@ -0,0 +1,15 @@
+_base_ = './htc_hrnet.py'
+model = dict(
+    backbone=dict(
+        type='HRNet',
+        extra=dict(
+            stage2=dict(num_channels=(40, 80)),
+            stage3=dict(num_channels=(40, 80, 160)),
+            stage4=dict(num_channels=(40, 80, 160, 320))),
+        init_cfg=dict(
+            type='Pretrained', checkpoint='open-mmlab://msra/hrnetv2_w40')),
+    neck=dict(type='HRFPN', in_channels=[40, 80, 160, 320], out_channels=256))
+
+
+lr_config = dict(step=[24, 27])
+runner = dict(type='EpochBasedRunner', max_epochs=28)
